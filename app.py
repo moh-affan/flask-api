09ef0047d00dae4676e77abc9e797b4fe59e55db
Moh. Affan <b4affan@gmail.com>
2020-03-09 14:40:20 +0700

    api with sqlalchemy

--- app.py
+++ app.py
@@ -1,21 +1,63 @@
-from flask import (
-    Flask,
-    render_template
-)
-app = Flask(__name__, template_folder="templates")
+import datetime
+import logging
 
-# Create a URL route in our application for "/"
-@app.route('/')
-def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
+import connexion
+from connexion import NoContent
 
-    :return:        the rendered template 'home.html'
-    """
-    return render_template('home.html')
+import orm
+
+db_session = None
 
 
-# If we're running in stand alone mode, run the application
+def get_pets(limit, animal_type=None):
+    q = db_session.query(orm.Pet)
+    if animal_type:
+        q = q.filter(orm.Pet.animal_type == animal_type)
+    return [p.dump() for p in q][:limit]
+
+
+def get_pet(pet_id):
+    pet = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
+    return pet.dump() if pet is not None else ('Not found', 404)
+
+
+def put_pet(pet_id, pet):
+    p = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
+    pet['id'] = pet_id
+    if p is not None:
+        logging.info('Updating pet %s..', pet_id)
+        p.update(**pet)
+    else:
+        logging.info('Creating pet %s..', pet_id)
+        pet['created'] = datetime.datetime.utcnow()
+        db_session.add(orm.Pet(**pet))
+    db_session.commit()
+    return NoContent, (200 if p is not None else 201)
+
+
+def delete_pet(pet_id):
+    pet = db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).one_or_none()
+    if pet is not None:
+        logging.info('Deleting pet %s..', pet_id)
+        db_session.query(orm.Pet).filter(orm.Pet.id == pet_id).delete()
+        db_session.commit()
+        return NoContent, 204
+    else:
+        return NoContent, 404
+
+
+logging.basicConfig(level=logging.INFO)
+db_session = orm.init_db('sqlite:///:memory:')
+app = connexion.FlaskApp(__name__)
+app.add_api('my_api.yaml')
+
+application = app.app
+
+
+@application.teardown_appcontext
+def shutdown_session(exception=None):
+    db_session.remove()
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080, use_reloader=False, threaded=False)
